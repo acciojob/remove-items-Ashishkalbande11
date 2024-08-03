12ebfbd1dbcd8267ac48f36238a6a1347c85986d
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-08-03 15:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded' function(){
+	const button = document.querySelector('input[type="button"]');
+
+	button.addEventListener("click", function(){
+		const colorSelect = document.getElementById('colorSelect');
+		const selectedIndex = colorSelect.selectedIndex;
+
+		if(selectedIndex !== -1){
+			colorSelect.options[selectedIndex].remove();
+		}
+	});
+});
